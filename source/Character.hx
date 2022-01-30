1077ea5978ede7f2452e9a32ae701e1ae1b912d5
Pyram1d <76640329+Pyram1d@users.noreply.github.com>
2022-01-30 15:02:13 -0600

    Bugfixes

--- source/Character.hx
+++ source/Character.hx
@@ -130,6 +130,12 @@ class Character extends FlxSprite
 			case 'gf-sus':
 				tex = Paths.getSparrowAtlas('GFs/GF_sus', 'shared', true);
 				frames = tex;
+				animation.addByIndices('danceLeft', 'GF Dancing Beat', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
+				animation.addByIndices('danceRight', 'GF Dancing Beat', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
+
+				loadOffsetFile('gf');
+
+				playAnim('danceRight');
 
 			case 'gf-amy':
 				tex = Paths.getSparrowAtlas('GFs/GF_amy', 'shared', true);
